5e85eae44673ce8cf129f1f205660468045e0dcb
FranCarrascosaMS <fran@metrica-sports.com>
2019-06-06 11:05:42 +0200

    LibVLC libraries created by ourselves linking definitions to DLLs

--- lib/msvc.bat
+++ lib/msvc.bat
@@ -1,5 +1,9 @@
 @echo off
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+
+mkdir msvc
+
 lib /NOLOGO /def:libvlc.def /out:msvc\libvlc.lib /machine:x86
 lib /NOLOGO /def:libvlc.def /out:msvc\libvlc.x64.lib /machine:x64
 
